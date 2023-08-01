0f441f24f86a3c0c41d393fdb2440b97fc9d8621
SUNIDHIJOHRI1234 <119292749+SUNIDHIJOHRI1234@users.noreply.github.com>
2023-08-01 17:06:47 +0530

    Update UserListCtl.java

--- Project_4/Project_4/src/main/java/in/co/rays/Project_4/Controller/UserListCtl.java
+++ Project_4/Project_4/src/main/java/in/co/rays/Project_4/Controller/UserListCtl.java
@@ -23,7 +23,7 @@ import in.co.rays.Project_4.Utility.ServletUtility;
  * User List functionality Controller. Performs operation for list, search and
  * delete operations of User
  * 
- * @author Darshan
+ * @author sunidhi johri
  *
  */
 @WebServlet(name = "UserListCtl", urlPatterns = { "/ctl/UserListCtl" })
